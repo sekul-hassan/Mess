090dcd54f08268ed5ccbc4e66bbb66ad519384e6
sekul <sekulhassan3031@gmail.com>
2023-05-10 11:19:09 +0600

    Backend is processing

--- src/main/java/com/messmanagementback/Controller/Login.java
+++ src/main/java/com/messmanagementback/Controller/Login.java
@@ -3,10 +3,7 @@ import com.messmanagementback.Model.MessInfo;
 import com.messmanagementback.Service.MessInfoService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,11 +17,9 @@ public class Login {
     private HttpSession session;
 
     @GetMapping("/login")
-    public MessInfo login(@RequestBody MessInfo messInfo){
+    public MessInfo login(@RequestParam("messId") String messId,@RequestParam("messPassword") String messPassword){
         boolean ok = false;
         List<MessInfo> allMember = messInfoService.getAllMess();
-        String messId = messInfo.getMessId();
-        String messPassword = messInfo.getMessPassword();
         for(MessInfo member:allMember){
             if(Objects.equals(member.getMessId(), messId) && Objects.equals(member.getMessPassword(), messPassword)){
                 ok = true;
